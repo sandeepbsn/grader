1d678a2cdde18ad0084b523b4a52a9bd3b447049
Sandeep Babu <sandeep.bsn@gmail.com>
2021-06-10 16:40:09 +0530

    feat: added build version

---
+++ build/precache-manifest.c96629b974d696148d47dbfe808834e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4bcd20621de054ea237e2efe24e240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5545670d7617aceead",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "cfa515f10274425c07cc",
+    "url": "/static/css/main.a1ca12e1.chunk.css"
+  },
+  {
+    "revision": "6c5545670d7617aceead",
+    "url": "/static/js/2.1c3c5e51.chunk.js"
+  },
+  {
+    "revision": "b74b435b32af732599fcb49a42b44436",
+    "url": "/static/js/2.1c3c5e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa515f10274425c07cc",
+    "url": "/static/js/main.241f243d.chunk.js"
+  },
+  {
+    "revision": "a9a75cacb82441679a8e",
+    "url": "/static/js/runtime-main.192d0565.js"
+  }
+]);
